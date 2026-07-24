e6be473ee58f1304fff6f2944cc6a8b2f70c9b84
Jaime Victoriano <xaxo.victoriano@gmail.com>
2026-07-24 17:19:16 -0400

    refactor(item): owner -> holder to avoid exceptions

--- items/item.gd
+++ items/item.gd
@@ -4,41 +4,41 @@ extends Node
 # @class abstract Item
 # @desc aimed to be the base of every Item
 
-# @attribute owner
-# @desc owner of this item
+# @attribute holder
+# @desc holder of this item
 @onready
-var owner: Entity = null
+var holder: Entity = null
 
 # @attribute was_equiped
-# @desc checks if the item was already picked up by any `owner`
+# @desc checks if the item was already picked up by any `holder`
 var was_equiped: bool = false
 
-func _equip(owner: Entity) -> void:
+func _equip(holder: Entity) -> void:
 	if not was_equiped:
 		was_equiped = true
-		on_first_pickup(owner)
-	on_equip(owner)
-	apply_effect(owner)
+		on_first_pickup(holder)
+	on_equip(holder)
+	apply_effect(holder)
 
-func _remove(owner: Entity) -> void:
-	on_unequip(owner)
+func _remove(holder: Entity) -> void:
+	on_unequip(holder)
 
 # @func virtual on_first_pickup
 # @desc This function triggers when picking the `Item` instance up for the first time
-func on_first_pickup(owner: Entity) -> void:
+func on_first_pickup(holder: Entity) -> void:
 	pass
 
 # @func virtual on_equip
 # @desc This function triggers every time a `Player` picks up this `Item` instance
-func on_equip(owner: Entity) -> void:
+func on_equip(holder: Entity) -> void:
 	pass
 
 # @func virtual apply_effect
 # @desc This function triggers immediatly after `on_equip`
-func apply_effect(owner: Entity) -> void:
+func apply_effect(holder: Entity) -> void:
 	pass
 
 # @func virtual on_unequip
 # @desc This function triggers when removing this `Item` instance
-func on_unequip(owner: Entity) -> void:
+func on_unequip(holder: Entity) -> void:
 	pass
